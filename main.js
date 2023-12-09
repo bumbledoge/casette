d7f0b4311b35b16ae189aca73366a7f8acf57e82
Cristi Gujan <dariusppopp@gmail.com>
2023-12-09 12:57:56 +0200

    blocky navbar

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import gsap from "gsap";
+
+const raycaster = new THREE.Raycaster();
+let currentIntersect = null,
+  previousIntersect = null,
+  thirdTry = null,
+  pressedButton = null;
 
 const axesHelper = new THREE.AxesHelper(20);
 const marimi = {
@@ -32,8 +39,10 @@ const button = new THREE.Mesh(
 );
 
 const buttons = [];
-for (let i = 0; i <= 8; i++) {
+for (let i = 0; i < 8; i++) {
   const button1 = button.clone();
+  const clonedMaterial = new THREE.MeshStandardMaterial({ color: "white" });
+  button1.material = clonedMaterial;
 
   button1.position.x += (buttonWidth + 0.05) * i;
   buttons.push(button1);
@@ -56,6 +65,28 @@ scene.add(light2);
 /**
  * Renders and animation
  */
+window.addEventListener("resize", () => {
+  // Update sizes
+  marimi.width = window.innerWidth;
+  marimi.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = marimi.width / marimi.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(marimi.width, marimi.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+const mouse = new THREE.Vector2();
+
+window.addEventListener("mousemove", (event) => {
+  mouse.x = (event.clientX / marimi.width) * 2 - 1;
+  mouse.y = -(event.clientY / marimi.height) * 2 + 1;
+
+  // console.log(mouse);
+});
 
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(marimi.width, marimi.height);
@@ -64,11 +95,62 @@ renderer.render(scene, camera);
 // const controls = new OrbitControls(camera, renderer.domElement);
 // controls.update();
 
-const tick = () => {
-  // console.log(camera.position);
-  // console.log(camera.rotation);
+window.addEventListener("click", () => {
+  if (currentIntersect) {
+    gsap.to(currentIntersect.object.position, {
+      duration: 0.5,
+      y: -0.5,
+      ease: "elastic.out(1.5,1)",
+    });
+    pressedButton &&
+      gsap.to(pressedButton.object.position, {
+        duration: 0.5,
+        y: 0,
+        ease: "elastic.out(1.5,1)",
+      });
+    pressedButton = currentIntersect;
+  }
+});
 
-  // controls.update();
+const tick = () => {
+  raycaster.setFromCamera(mouse, camera);
+
+  const intersects = raycaster.intersectObjects(buttons);
+
+  // for (const intersect of intersects) {
+  //   intersect.object.material.color.set("#0000ff");
+  // }
+
+  if (intersects.length) {
+    if (!currentIntersect) {
+      // console.log("mouse enter");
+    }
+    if (currentIntersect) {
+      // currentIntersect.object.material.color.set("white");
+      // pressedButton &&
+      //   pressedButton.object.position !== currentIntersect.object.position &&
+      //   (currentIntersect.object.position.y = 0);
+    }
+    // console.log(currentIntersect);
+    thirdTry = currentIntersect;
+    currentIntersect = intersects[0];
+    // pressedButton &&
+    // currentIntersect.object.material.color.set("#0000ff");
+    // pressedButton &&
+    // pressedButton.object.position !== currentIntersect.object.position &&
+    // (currentIntersect.object.position.y = -0.05);
+  } else {
+    if (currentIntersect) {
+      // console.log("mouse leave");
+      // currentIntersect.object.material.color.set("white");
+      // pressedButton &&
+      //   pressedButton.object.position !== currentIntersect.object.position &&
+      //   (currentIntersect.object.position.y = 0);
+    }
+
+    currentIntersect = null;
+  }
+
   requestAnimationFrame(tick);
   renderer.render(scene, camera);
 };
